57e4de2c2fb51507b56c71d492007faa114c4467
eyrim <marfxedbot@gmail.com>
2024-12-10 22:02:45 +0000

    Add page with route

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
+  const ProfilePage({super.key});
+
   @override
   Widget build(BuildContext context) {
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Go Home'),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+    );
   }
 }

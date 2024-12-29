c6c81945996ada2b4e0097a6b48bf942bd5a41e8
eyrim <marfxedbot@gmail.com>
2024-12-29 22:49:53 +0000

    Add comments and convenience method to unit

--- lib/model/unit.dart
+++ lib/model/unit.dart
@@ -1,19 +1,29 @@
-import 'package:click_in_wardrobe/functional_interfaces.dart';
 import 'package:click_in_wardrobe/math/double_extensions.dart';
 
+/// A unit of measurement, for example a centimetre.
 enum Unit {
   centimeters(
       convertFromMillimeters: UnitConversions.centimetersFromMillimeters),
-  millimeters(convertFromMillimeters: FunctionUtils.identity),
+  millimeters(convertFromMillimeters: UnitConversions.identity),
   inches(convertFromMillimeters: UnitConversions.inchesFromMillimeters);
 
-  const Unit({required this.convertFromMillimeters});
+  const Unit({required convertFromMillimeters})
+      : _convertFromMillimeters = convertFromMillimeters;
 
-  final Function(double) convertFromMillimeters;
+  final double Function(double) _convertFromMillimeters;
+
+  /// Convenience method to convert a value in millimetres to the current unit
+  double convertFromMillimeters(final double millis) {
+    return _convertFromMillimeters.call(millis);
+  }
 }
 
 class UnitConversions {
-  static double inchesFromMillimeters(final double millis) => (millis / 25.4).toDoubleAsFixed(3);
+  static double inchesFromMillimeters(final double millis) =>
+      (millis / 25.4).toDoubleAsFixed(3);
 
-  static double centimetersFromMillimeters(final double millis) => (millis / 10).toDoubleAsFixed(3);
+  static double centimetersFromMillimeters(final double millis) =>
+      (millis / 10).toDoubleAsFixed(3);
+
+  static double identity(final double millis) => millis;
 }

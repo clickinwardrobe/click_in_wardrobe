714acbab4f6b6bc4e3f7bf19e70362c4c2dcfec3
Eyrim <62777325+Eyrim@users.noreply.github.com>
2024-12-29 23:17:18 +0000

    Fix test to use new Measurement signature (#14)

--- test/model/measurement_test.dart
+++ test/model/measurement_test.dart
@@ -7,22 +7,28 @@ void main() {
     late Measurement asMillis;
 
     setUp(() {
-        asMillis = Measurement(displayName: "test_measurement", valueAsMillis: 500);
+      asMillis = Measurement(
+          displayName: "test_measurement",
+          valueAsMillis: 500,
+          unit: Unit.millimeters);
     });
-    
-    test('inches given a normal value', () {
-        const double expectedInches = 19.685;
-        final double actual = asMillis.convertTo(Unit.inches);
 
-        expect(actual, expectedInches);
+    test('inches given a normal value', () {
+      const double expectedInches = 19.685;
+      final double actual = asMillis.convertTo(Unit.inches);
+
+      expect(actual, expectedInches);
     });
 
     test('inches given 0', () {
-        final Measurement asMillis = Measurement(displayName: "shadowing asMillis so it can be 0", valueAsMillis: 0);
-        const double expectedInches = 0;
-        final double actual = asMillis.convertTo(Unit.inches);
+      final Measurement asMillis = Measurement(
+          displayName: "shadowing asMillis so it can be 0",
+          valueAsMillis: 0,
+          unit: Unit.millimeters);
+      const double expectedInches = 0;
+      final double actual = asMillis.convertTo(Unit.inches);
 
-        expect(actual, expectedInches);
+      expect(actual, expectedInches);
     });
   });
 }

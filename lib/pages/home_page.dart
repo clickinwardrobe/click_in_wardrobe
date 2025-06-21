0fd3ca67b8be79d21c8d5f073152d25326b99b05
Eyrim <62777325+Eyrim@users.noreply.github.com>
2025-06-21 21:32:58 +0100

    Remove old app state file (#17)
    this is from the default app

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,24 +1,12 @@
 import 'package:click_in_wardrobe/pages/profile_page.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
-import '../my_app_state.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-
     return Scaffold(
       body: Column(
         children: [
-          Text('A random idea:'),
-          Text(appState.current.asLowerCase),
-          ElevatedButton(
-              onPressed: () {
-                print(appState.current);
-              },
-              child: Text('Next')),
           ElevatedButton(
               onPressed: () {
                 Navigator.push(
@@ -26,7 +14,7 @@ class HomePage extends StatelessWidget {
                     MaterialPageRoute(
                         builder: (context) => ProfilePage()));
               },
-              child: Text('Go to profile')),
+              child: const Text('My Profile')),
         ],
       ),
     );
